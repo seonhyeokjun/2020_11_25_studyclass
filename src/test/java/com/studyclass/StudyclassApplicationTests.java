0925a7c473984f1213c0aa206a01a85d1329c263
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-02 20:55:37 +0900

    Initial commit

---
+++ src/test/java/com/studyclass/StudyclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
