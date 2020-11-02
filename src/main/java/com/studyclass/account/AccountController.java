4a124e0af37f99366afa92c5885bdb17c9702f21
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-02 22:02:04 +0900

    계정 컨트롤러 생성 및 테스트

---
+++ src/main/java/com/studyclass/account/AccountController.java
@@ -0,0 +1,14 @@
+package com.studyclass.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping(value = "/sign-up")
+    public String signupForm(Model model){
+        return "account/sign-up";
+    }
+}
