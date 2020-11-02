2cbd8842c8d077d69c9da93ebdc0b10398aaecb6
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-03 03:02:23 +0900

    회원가입 서브밋 검증 및 처리

--- src/main/java/com/studyclass/domain/Account.java
+++ src/main/java/com/studyclass/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -57,4 +58,8 @@ public class Account {
     private boolean studyUpdatedByEmail;
 
     private boolean studyUpdatedByWeb;
+
+    public void generateEmailCheckToken() {
+        this.emailCheckToken = UUID.randomUUID().toString();
+    }
 }
