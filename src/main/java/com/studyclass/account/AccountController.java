0552a11a59707153ab74ad095f8fce2f405b0726
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-04 03:22:11 +0900

    이메일 인증확인

--- src/main/java/com/studyclass/account/AccountController.java
+++ src/main/java/com/studyclass/account/AccountController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +22,8 @@ public class AccountController {
     private final SignUpFormValidator signUpFormValidator;
 
     private final AccountService accountService;
+
+    private final AccountRepository accountRepository;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder){
@@ -42,6 +45,27 @@ public class AccountController {
         accountService.processNewAccount(signUpForm);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/check-email-token")
+    public String checkEmailToken(String token, String email, Model model){
+        Account account = accountRepository.findByEmail(email);
+        String view = "account/checked-email";
+        if (account == null){
+            model.addAttribute("error", "wrong.email");
+            return view;
+        }
+
+        if (!account.getEmailCheckToken().equals(token)){
+            model.addAttribute("error", "wrong.token");
+            return view;
+        }
+
+        account.setEmailVerified(true);
+        account.setJoinedAt(LocalDateTime.now());
+        model.addAttribute("numberOfUser", accountRepository.count());
+        model.addAttribute("nickName", account.getNickname());
+        return view;
     }
 
 }
--- src/main/java/com/studyclass/mail/ConsoleMailSender.java
+++ src/main/java/com/studyclass/mail/ConsoleMailSender.java
@@ -1,4 +1,4 @@
-package com.studyclass;
+package com.studyclass.mail;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
