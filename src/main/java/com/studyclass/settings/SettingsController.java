9deeab72350102b2ea9d75801694d1409cb26f16
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-12 20:04:22 +0900

    수정 테스트

--- src/main/java/com/studyclass/settings/SettingsController.java
+++ src/main/java/com/studyclass/settings/SettingsController.java
@@ -18,9 +18,8 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class SettingsController {
 
-    private static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
-
-    private static final String SETTINGS_PROFILE_URL = "settings/profile";
+    static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
+    static final String SETTINGS_PROFILE_URL = "/settings/profile";
 
     private final AccountService accountService;
 
@@ -28,10 +27,11 @@ public class SettingsController {
     public String profileUpdateForm(@CurrentUser Account account, Model model){
         model.addAttribute(account);
         model.addAttribute(new Profile(account));
+
         return SETTINGS_PROFILE_VIEW_NAME;
     }
 
-    @PostMapping("/settings/profile")
+    @PostMapping(SETTINGS_PROFILE_URL)
     public String updateProfile(@CurrentUser Account account, @Valid Profile profile, Errors errors, Model model,
                                 RedirectAttributes attributes){
         if (errors.hasErrors()){
@@ -41,6 +41,6 @@ public class SettingsController {
 
         accountService.updateProfile(account, profile);
         attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
-        return "redirect:/" + SETTINGS_PROFILE_URL;
+        return "redirect:" + SETTINGS_PROFILE_URL;
     }
 }
