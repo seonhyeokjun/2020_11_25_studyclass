fdd27dd9daef43f001ea37a69014d06cadbe5b5b
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-12 23:49:51 +0900

    이미지 업로드 및 수정에 필요한 npm 라이브러리 설치 및 기능구현

--- src/main/java/com/studyclass/account/AccountService.java
+++ src/main/java/com/studyclass/account/AccountService.java
@@ -89,6 +89,7 @@ public class AccountService implements UserDetailsService {
         account.setOccupation(profile.getOccupation());
         account.setLocation(profile.getLocation());
         account.setBio(profile.getBio());
+        account.setProfileImage(profile.getProfileImage());
         accountRepository.save(account);
     }
 }
