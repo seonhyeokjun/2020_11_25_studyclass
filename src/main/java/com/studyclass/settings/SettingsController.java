c1e85f25fae487a539a9092f3e76132f17c8a4f2
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-14 01:45:06 +0900

    관심주제 조회

--- src/main/java/com/studyclass/settings/SettingsController.java
+++ src/main/java/com/studyclass/settings/SettingsController.java
@@ -20,6 +20,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -135,6 +138,8 @@ public class SettingsController {
     @GetMapping(SETTINGS_TAGS_URL)
     public String updateTags(@CurrentUser Account account, Model model){
         model.addAttribute(account);
+        Set<Tag> tags = accountService.getTags(account);
+        model.addAttribute("tags", tags.stream().map(Tag::getTitle).collect(Collectors.toList()));
         return SETTINGS_TAGS_VIEW_NAME;
     }
 
