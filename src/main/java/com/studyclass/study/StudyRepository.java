d4a69785947982df35ad632a4ad126fc1f3ac5f3
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-16 21:36:19 +0900

    모임 개설 관련 기능 구현

---
+++ src/main/java/com/studyclass/study/StudyRepository.java
@@ -0,0 +1,12 @@
+package com.studyclass.study;
+
+import com.studyclass.domain.Study;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface StudyRepository extends JpaRepository<Study, Long> {
+    boolean existsByPath(String path);
+
+    Study findByPath(String path);
+}
---
+++ src/main/java/com/studyclass/study/StudyService.java
@@ -0,0 +1,21 @@
+package com.studyclass.study;
+
+import com.studyclass.domain.Account;
+import com.studyclass.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StudyService {
+
+    private final StudyRepository studyRepository;
+
+    public Study createNewStudy(Study study, Account account) {
+        Study newStudy = studyRepository.save(study);
+        newStudy.addManager(account);
+        return newStudy;
+    }
+}
