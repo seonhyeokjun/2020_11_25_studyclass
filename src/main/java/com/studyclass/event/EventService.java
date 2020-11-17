7f6f49510cb2aa4dbd83176c70d6268c0693a986
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-18 03:31:07 +0900

    event 수정

--- src/main/java/com/studyclass/event/EventService.java
+++ src/main/java/com/studyclass/event/EventService.java
@@ -3,7 +3,9 @@ package com.studyclass.event;
 import com.studyclass.domain.Account;
 import com.studyclass.domain.Event;
 import com.studyclass.domain.Study;
+import com.studyclass.event.form.EventForm;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +18,16 @@ public class EventService {
 
     private final EventRepository eventRepository;
 
+    private final ModelMapper modelMapper;
+
     public Event createEvent(Event event, Study study, Account account) {
         event.setCreatedBy(account);
         event.setCreatedDateTime(LocalDateTime.now());
         event.setStudy(study);
         return eventRepository.save(event);
+    }
+
+    public void updateEvent(Event event, EventForm eventForm) {
+        modelMapper.map(eventForm, event);
     }
 }
--- src/main/java/com/studyclass/event/validator/EventValidator.java
+++ src/main/java/com/studyclass/event/validator/EventValidator.java
@@ -1,5 +1,6 @@
 package com.studyclass.event.validator;
 
+import com.studyclass.domain.Event;
 import com.studyclass.event.form.EventForm;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -42,5 +43,11 @@ public class EventValidator implements Validator {
     private boolean isNotValidEndDateTime(EventForm eventForm) {
         LocalDateTime endDateTime = eventForm.getEndDateTime();
         return endDateTime.isBefore(eventForm.getStartDateTime()) || endDateTime.isBefore(eventForm.getEndEnrollmentDateTime());
+    }
+
+    public void validateUpdateForm(EventForm eventForm, Event event, Errors errors) {
+        if (eventForm.getLimitOfEnrollments() < event.getNumberOfAcceptedEnrollments()) {
+            errors.rejectValue("limitOfEnrollments", "wrong.value", "확인된 참기 신청보다 모집 인원 수가 커야 합니다.");
+        }
     }
 }
