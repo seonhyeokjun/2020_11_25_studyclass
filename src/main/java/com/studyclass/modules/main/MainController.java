4ca39f6b4c1dc5d603e07daca23e27a34d3199ec
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-24 20:34:42 +0900

    로그인 전 화면, 코드 리팩토링

--- src/main/java/com/studyclass/modules/main/MainController.java
+++ src/main/java/com/studyclass/modules/main/MainController.java
@@ -28,7 +28,7 @@ public class MainController {
         if (account!=null){
             model.addAttribute(account);
         }
-
+        model.addAttribute("studyList", studyRepository.findFirst9ByPublishedAndClosedOrderByPublishedDateTimeDesc(true, false));
         return "index";
     }
 
