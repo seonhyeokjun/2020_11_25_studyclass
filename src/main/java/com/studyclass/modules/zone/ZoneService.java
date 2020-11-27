ce1e78b9a2e218b068d0fbadf67c0f592e2e205b
seonhyeogjun <tjsgurwns@knou.ac.kr>
2020-11-27 09:02:39 +0900

    배포시 리소스 하기위해 코드수정

--- src/main/java/com/studyclass/modules/zone/ZoneService.java
+++ src/main/java/com/studyclass/modules/zone/ZoneService.java
@@ -23,7 +23,7 @@ public class ZoneService {
     @PostConstruct
     public void initZoneData() throws IOException {
         if (zoneRepository.count() == 0){
-            Resource resource = new ClassPathResource("zones_kr.csv");
+            ClassPathResource resource = new ClassPathResource("zones_kr.csv");
             List<Zone> zoneList = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8).stream()
                         .map(line -> {
                             String[] split = line.split(",");
